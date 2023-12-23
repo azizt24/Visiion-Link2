e8a6f2d59bf475b52b06e1149e86ad4d09e4fa38
azizt24 <78258864+azizt24@users.noreply.github.com>
2023-12-23 02:33:13 +0200

    Update MeetRoom.jsx

--- Vision-Link/client/vite-project/src/pages/MeetRoom.jsx
+++ Vision-Link/client/vite-project/src/pages/MeetRoom.jsx
@@ -30,73 +30,36 @@ const MeetRoom = () => {
   };
 
   useEffect(() => {
-    console.log('Requesting to join room:', { userId, roomId: id });
     socket.emit('request-to-join-room', { userId, roomId: id });
-
-    console.log('Joining room:', { userId, roomId: id });
     socket.emit('join-room', { userId, roomId: id });
-
-    console.log('Listening for user-joined event');
     socket.on("user-joined", handleUserJoin);
-
-    console.log('Getting participants:', { roomId: id });
     socket.emit('get-participants', { roomId: id });
-
-    console.log('Listening for participants-list event');
     socket.on("participants-list", ({ usernames, roomName }) => {
-      console.log('Participants list received:', { usernames, roomName });
       setParticipants(usernames);
       setRoomName(roomName);
     });
 
-    console.log('Listening for user-requested-to-join event');
     socket.on("user-requested-to-join", handleUserRequestedToJoin);
   }, [socket]);
 
   useEffect(() => {
     const initAgora = async (name) => {
-      client.on("user-published", async (user, mediaType) => {
-        await client.subscribe(user, mediaType);
-        if (mediaType === "video") {
-          setUsers((prevUsers) => [...prevUsers, user]);
-        }
-        if (mediaType === "audio") {
-          user.audioTrack.play();
-        }
-      });
-
-      client.on("user-unpublished", (user, mediaType) => {
-        if (mediaType === "audio") {
-          if (user.audioTrack) user.audioTrack.stop();
-        }
-        if (mediaType === "video") {
-          setUsers((prevUsers) => prevUsers.filter((User) => User.uid !== user.uid));
-        }
-      });
-
-      client.on("user-left", (user) => {
-        console.log('User left room:', { userId: user.uid, roomId: id });
-        socket.emit("user-left-room", { userId: user.uid, roomId: id });
-        setUsers((prevUsers) => prevUsers.filter((User) => User.uid !== user.uid));
-      });
-
       try {
-        // Convert userId to a numeric value within the valid range (0 to 10000)
         const numericUserId = parseInt(userId, 10) % 10000;
-        
+
         console.log('Joining Agora channel:', { appId: config.appId, name, token: config.token, numericUserId });
         await client.join(config.appId, name, config.token, numericUserId);
+
+        if (tracks) {
+          console.log('Publishing tracks:', tracks);
+          await client.publish([tracks[0], tracks[1]]);
+        }
+
+        console.log('Setting start to true');
+        setStart(true);
       } catch (error) {
         console.error('Error joining Agora channel:', error);
       }
-
-      if (tracks) {
-        console.log('Publishing tracks:', tracks);
-        await client.publish([tracks[0], tracks[1]]);
-      }
-
-      console.log('Setting start to true');
-      setStart(true);
     };
 
     if (ready && tracks) {
