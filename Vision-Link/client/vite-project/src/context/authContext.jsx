240e19a48a81d9ce9823c9d34ff4a5290785a006
azizt24 <78258864+azizt24@users.noreply.github.com>
2023-12-25 01:32:33 +0200

    Update authContext.jsx

--- Vision-Link/client/vite-project/src/context/authContext.jsx
+++ Vision-Link/client/vite-project/src/context/authContext.jsx
@@ -9,7 +9,7 @@ export const AuthContextProvider = ({ children }) => {
 
   const login = async (inputs) => {
     try {
-      const res = await axios.post('http://localhost:4000/auth/login', inputs);
+      const res = await axios.post('https://vision-link-api.onrender.com/auth/login', inputs);
       const { token, user } = res.data;
 
       localStorage.setItem('userToken', token);
